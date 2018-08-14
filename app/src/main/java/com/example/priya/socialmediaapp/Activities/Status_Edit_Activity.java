66cfed0342608c8898197da353bb719279be451a
priyaVora <prvora89@gmail.com>
2018-08-14 08:34:25 -0600

    Update: Created Edit Status Page

---
+++ app/src/main/java/com/example/priya/socialmediaapp/Activities/Status_Edit_Activity.java
@@ -0,0 +1,31 @@
+package com.example.priya.socialmediaapp.Activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.priya.socialmediaapp.R;
+
+public class Status_Edit_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status__edit_);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
