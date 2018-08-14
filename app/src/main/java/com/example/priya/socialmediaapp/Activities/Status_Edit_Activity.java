fb3bfbdb807cabbe1612a240d36dd52e1df77d45
priyaVora <prvora89@gmail.com>
2018-08-14 08:56:03 -0600

    Update

--- app/src/main/java/com/example/priya/socialmediaapp/Activities/Status_Edit_Activity.java
+++ app/src/main/java/com/example/priya/socialmediaapp/Activities/Status_Edit_Activity.java
@@ -1,10 +1,13 @@
 package com.example.priya.socialmediaapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.priya.socialmediaapp.R;
@@ -16,7 +19,9 @@ public class Status_Edit_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status__edit_);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("My status");
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,4 +33,20 @@ public class Status_Edit_Activity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_signout) {
+            startActivity(new Intent(Status_Edit_Activity.this, LoginActivity.class));
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/priya/socialmediaapp/Model/tab_status.java
+++ app/src/main/java/com/example/priya/socialmediaapp/Model/tab_status.java
@@ -1,11 +1,14 @@
 package com.example.priya.socialmediaapp.Model;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.priya.socialmediaapp.Activities.Status_Edit_Activity;
 import com.example.priya.socialmediaapp.R;
 
 /**
@@ -13,10 +16,21 @@ import com.example.priya.socialmediaapp.R;
  */
 
 public class tab_status extends Fragment {
+
+    private Button menu_button;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.status, container, false);
+        final View rootView = inflater.inflate(R.layout.status, container, false);
+        menu_button = rootView.findViewById(R.id.status_menu_button);
+        menu_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(rootView.getContext(), Status_Edit_Activity.class));
+
+            }
+        });
         return rootView;
     }
 }
